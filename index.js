d0cc3f967f0399e9b2d0f458e5b8787cc5eea68f
BintangSaputra <bintangcbs26@gmail.com>
2019-05-23 18:56:53 +0700

    styling card

--- index.js
+++ index.js
@@ -11,9 +11,9 @@ const displayContent = products => {
     let productDiv = `
     <div class="card">
             <img src="${product.image}" class="card-img-top" alt="..." />
-            <div class="card-body">
+            <div id="card-home" class="card-body">
               <h5 class="card-title">${product.name}</h5>
-              <p class="card-text">${product.color}</p>
+              <p id="color-product" class="card-text">${product.color}</p>
               <p class="card-text">
                ${product.price}
               </p>
