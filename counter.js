2c5b5891128ec58c88c7535d1dbede63fbbd7e93
BintangSaputra <bintangcbs26@gmail.com>
2019-05-24 14:31:45 +0700

    refactor quantity input

--- counter.js
+++
@@ -1,5 +0,0 @@
-let counterField = document.getElementById("counterField"),
-  count = 0;
-
-const minButton = document.getElementById("minButton");
-const plusButton = document.getElementById("plusButton");
