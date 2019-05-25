5f4c2c82c6b5f862dfe3782d1188dd48efaefd32
BintangSaputra <bintangcbs26@gmail.com>
2019-05-25 18:59:14 +0700

    test gtags.js

---
+++ gtags.js
@@ -0,0 +1,25 @@
+gtag("event", "view_item_list", {
+  items: [
+    {
+      id: "1",
+      name: "Product A",
+      color: "BLACK",
+      price: "200000",
+      position: 1
+    },
+    {
+      id: "2",
+      name: "Product B",
+      color: "BLACK",
+      price: "200000",
+      position: 2
+    },
+    {
+      id: "3",
+      name: "Product C",
+      color: "BLACK",
+      price: "200000",
+      position: 3
+    }
+  ]
+});
