d8cf9f415ed925a17790b21a578e7521e8676540
BintangSaputra <bintangcbs26@gmail.com>
2019-05-26 09:29:26 +0700

    add data

--- data.js
+++ data.js
@@ -1,18 +1,20 @@
 const products = [
   {
-    id: 1,
+    id: "S12",
     name: "Product A",
     color: "BLACK",
     price: 200000,
     image: "images/productthree.jpeg"
   },
   {
+    id: "S13",
     name: "Product B",
     color: "BLACK",
     price: 200000,
     image: "images/productfour.jpg"
   },
   {
+    id: "S14",
     name: "Product C",
     color: "BLACK",
     price: 200000,
